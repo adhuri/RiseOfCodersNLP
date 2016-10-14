8e2559bd314bc8b6e3550a1468dd3952853e090a
Chethan <cnanniv@ncsu.edu>
2016-10-14 04:11:46 -0400

    Fixed the json issue

--- alchemy_language_v1.py
+++ alchemy_language_v1.py
@@ -4,7 +4,8 @@ from watson_developer_cloud import AlchemyLanguageV1
 from roc import get_sentence,fix_xml,get_title
 from nlp import get_summary
 import os
-
+import os.path
+import codecs
 
 def generate_keywords_dict():
     global keywords_dict
@@ -34,14 +35,21 @@ def generate_keywords_dict():
 
 
         if filename.endswith(".xml"):
-            #fix_xml(directory+"/"+filename)
+            fix_xml(filename)
             title=get_title(filename)
+            print filename
             extracted_text=get_sentence(filename)
             try:
-                output_file.write(json.dumps(alchemy_language.combined(extracted_text, extract=combined_operations), indent=2))
-                output_file.close()
-                with open('output.json') as data_file:
-                    data = json.load(data_file)
+                if(not os.path.isfile(json_filename)):
+
+                    output_file.write(json.dumps(alchemy_language.combined(extracted_text, extract=combined_operations), indent=2))
+                    output_file.close()
+                    with open('output.json') as data_file:
+                        data = json.load(data_file)
+                else:
+                    with open(json_filename) as data_file:
+                        #data_file1=json.dumps(data_file)
+                        data = json.load((data_file))
 
                 for d in data["keywords"]:
                     temp_list_keys.append(d['text'])
